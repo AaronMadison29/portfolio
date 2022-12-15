19ec3a9376312029f988f759ecf24a9c6c68b4d1
AaronMadison29 <59708277+AaronMadison29@users.noreply.github.com>
2022-12-15 12:55:44 -0600

    Update Resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,7 +5,7 @@ export default class Resume extends React.Component
     render(){
         return(
             <div className="text-center navigation">
-                <a className={"link"} href="https://drive.google.com/file/d/1gaYZ94oHrvS-CpRgLniUo4C7ubzEWbtE/view?usp=sharing" target="_blank">
+                <a className={"link"} href="https://drive.google.com/file/d/1lR6bdSQSG7kqXSc28f9s5HIPJYj6UKDG/view?usp=share_link" target="_blank">
                     Resume
                 </a>
             </div>
